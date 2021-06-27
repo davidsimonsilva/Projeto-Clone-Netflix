103a5e8ff2de4b0a1f1f61e5527d7d2e67a21d65
David Simon <59507155+davidsimonsilva@users.noreply.github.com>
2021-06-27 10:17:13 -0300

    Clone Netflix 5
    Criação do filme em destaque com suas características visuais e descrições como:  Nome do filme, Avaliação, Ano que começo, Temporada(s), Descrição, Botão de Assistir e Minha Lista e o Gênero(s).
    
    Aspectos visuais para dar uma melhor sensação visual para gerar agrado e comodidade a pessoa que acesse o site.

--- netflixclone/src/components/MovieRow.js
+++ netflixclone/src/components/MovieRow.js
@@ -8,9 +8,9 @@ export default ({title, items}) => {
             <div className="movieRow--listarea">
                 <div className="movieRow--list">
                     {items.results.length > 0 && items.results.map((item, key)=>(
-                     <div className= "movieRow--item">
-                         <img src={`https://image.tmdb.org/t/p/w300${item.poster_path}`} alt="{item.original_title}" />
-                     </div>
+                    <div key={key} className= "movieRow--item">
+                        <img src={`https://image.tmdb.org/t/p/w300${item.poster_path}`} alt="{item.original_title}" />
+                    </div>
                      ))}
                 </div>
             </div>
